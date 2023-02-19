05a57f936de6fbf1fc27087ef4bbea25f3a6ec80
Victor <publicdomainvic@gmail.com>
2023-02-19 16:39:12 -0400

    Add OnExitToolStripMenuItemClick()

--- RenameHierarchy/MainForm.cs
+++ RenameHierarchy/MainForm.cs
@@ -274,7 +274,8 @@ namespace RenameHierarchy
         /// <param name="e">Event arguments.</param>
         private void OnExitToolStripMenuItemClick(object sender, EventArgs e)
         {
-            // TODO Add code
+            // Close program        
+            this.Close();
         }
     }
 }
